422af5d2ae000cc8f822015613fbd89a5cb085cd
mbyrd <mbyrd@trhc.com>
2021-08-15 10:09:51 -0400

    Only return mock data for this demo

--- src/services/frontEndService.js
+++ src/services/frontEndService.js
@@ -1,31 +1,12 @@
-import ServiceHelper from "../helper/serviceHelper";
 import JobsMock from "../mockData/jobsMock";
 import ProvidersMock from "../mockData/providersMock";
 
 const fetchJobs = async () => {
-  if (ServiceHelper.isLocalHost) {
-    try {
-      return await fetch(`http://localhost:3000/jobs`)
-        .then(r => r.json());
-    } catch (e) {
-      return `${e}`;
-    }
-  } else {
-    return JobsMock
-  }
+  return JobsMock.getJobsMock
 }
 
 const fetchProviders = async () => {
-  if (ServiceHelper.isLocalHost) {
-    try {
-      return await fetch(`http://localhost:3000/providers`)
-        .then(r => r.json());
-    } catch (e) {
-      return `${e}`;
-    }
-  } else {
-    return ProvidersMock
-  }
+  return ProvidersMock.getProvidersMock
 }
 
 const FrontEndService = {fetchJobs, fetchProviders};
